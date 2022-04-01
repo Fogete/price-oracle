c12fec6163569888c5c2594980bc107f38c301dc
Fogete <74245508+Fogete@users.noreply.github.com>
2022-04-01 20:01:04 +0200

    feat: uni library

--- contracts/libraries/UniswapV2OracleLibrary.sol
+++ contracts/libraries/UniswapV2OracleLibrary.sol
@@ -1,18 +1,18 @@
 pragma solidity >=0.5.0;
 
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "../interfaces/IUniswapV2Pair.sol";
 import "@uniswap/lib/contracts/libraries/FixedPoint.sol";
 
-// library with helper methods for oracles that are concerned with computing average prices
+// Library with helper methods for oracles that are concerned with computing average prices.
 library UniswapV2OracleLibrary {
   using FixedPoint for *;
 
-  // helper function that returns the current block timestamp within the range of uint32, i.e. [0, 2**32 - 1]
+  // Helper function that returns the current block timestamp within the range of uint32, i.e. [0, 2**32 - 1]
   function currentBlockTimestamp() internal view returns (uint32) {
     return uint32(block.timestamp % 2**32);
   }
 
-  // produces the cumulative price using counterfactuals to save gas and avoid a call to sync.
+  // Produces the cumulative price using counterfactuals to save gas and avoid a call to sync.
   function currentCumulativePrices(address pair)
     internal
     view
@@ -26,19 +26,19 @@ library UniswapV2OracleLibrary {
     price0Cumulative = IUniswapV2Pair(pair).price0CumulativeLast();
     price1Cumulative = IUniswapV2Pair(pair).price1CumulativeLast();
 
-    // if time has elapsed since the last update on the pair, mock the accumulated price values
+    // If time has elapsed since the last update on the pair, mock the accumulated price values.
     (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = IUniswapV2Pair(
       pair
     ).getReserves();
     if (blockTimestampLast != blockTimestamp) {
-      // subtraction overflow is desired
+      // Subtraction overflow is desired
       uint32 timeElapsed = blockTimestamp - blockTimestampLast;
-      // addition overflow is desired
-      // counterfactual
+      // Addition overflow is desired
+      // Counterfactual
       price0Cumulative +=
         uint(FixedPoint.fraction(reserve1, reserve0)._x) *
         timeElapsed;
-      // counterfactual
+      // Counterfactual
       price1Cumulative +=
         uint(FixedPoint.fraction(reserve0, reserve1)._x) *
         timeElapsed;
